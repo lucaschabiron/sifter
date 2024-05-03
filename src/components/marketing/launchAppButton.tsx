bb69e11f6095355eb0e9e1a0ba6607b908f221fc
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2024-05-03 23:33:33 +0900

    feat(landing): add link to login

--- src/components/marketing/launchAppButton.tsx
+++ src/components/marketing/launchAppButton.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { Button } from "../ui/button";
-import { toast } from "../ui/use-toast";
+import { useRouter } from "next/navigation";
 export function LaunchAppButton() {
-  const onClick = () => {
-    toast({
-      title: "Coming Soon",
-    });
-  };
+  const router = useRouter();
   return (
     <Button
-      onClick={() => onClick()}
+      onClick={() => router.push("/login")}
       className="text-gray-100 bg-gray-800/60 border-gray-400 border-2 rounded-lg hover:bg-gray-600/70 blured"
     >
       Launch App
