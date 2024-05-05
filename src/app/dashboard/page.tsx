56a03a45dccc263bf4dbc38e1ddbe488b2369ff1
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2024-05-05 23:59:21 +0900

    fix: change toast

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,7 +12,6 @@ export default async function DashboardPage() {
   return (
     <div className="flex h-screen bg-gray-950">
       <div className="m-auto w-96">
-        <CurrentSessionHelper />
         <h1 className="text-white text-2xl">Dashboard</h1>
       </div>
       <div className="absolute right-4 top-4">
