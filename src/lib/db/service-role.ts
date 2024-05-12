53e888a842a716c703a577f9a6754d3fa5f69d0a
Lucas Chabiron <84028247+lucaschabiron@users.noreply.github.com>
2024-05-12 10:30:00 +0900

    feat(db): add type support

--- src/lib/db/service-role.ts
+++ src/lib/db/service-role.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-
+import type { Database } from "./database.types";
 export function serviceRoleClient() {
-  return createClient(
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
